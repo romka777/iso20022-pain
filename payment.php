cf7a6f2ceb90886106b3ee985c2a39600d4a0e5f
Roman Gogolev <roman.gogolev@gmail.com>
2019-02-05 17:29:11 +0300

    add payments

--- payment.php
+++ payment.php
@@ -9,14 +9,8 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 use Money\Money;
-use Consilience\Pain001\FinancialInstitution\BIC;
-use Consilience\Pain001\Account\IBAN;
 use Consilience\Pain001\Message\CustomerCreditTransfer;
 use Consilience\Pain001\PaymentInformation\PaymentInformation;
-use Consilience\Pain001\Account\PostalAccount;
-use Consilience\Pain001\Address\StructuredPostalAddress;
-use Consilience\Pain001\TransactionInformation\BankCreditTransfer;
-use Consilience\Pain001\TransactionInformation\IS1CreditTransfer;
 use Consilience\Pain001\Address\UnstructuredPostalAddress;
 use Consilience\Pain001\OrganisationIdentification\Inn;
 use Consilience\Pain001\Account\GeneralAccount;
@@ -26,26 +20,7 @@ use Consilience\Pain001\TransactionInformation\ForeignCreditTransfer;
 use Consilience\Pain001\PaymentInformation\ServiceLevelCode;
 use Consilience\Pain001\TransactionInformation\PurposeProprietary;
 
-$transaction1 = new BankCreditTransfer(
-    'instr-001',
-    'e2e-001',
-    Money::CHF(130000), // CHF 1300.00
-    'Muster Transport AG',
-    new StructuredPostalAddress('Wiesenweg', '14b', '8058', 'Zürich-Flughafen', 'RU'),
-    new IBAN('CH51 0022 5225 9529 1301 C'),
-    new BIC('UBSWCHZH80A')
-);
-
-$transaction2 = new IS1CreditTransfer(
-    'instr-002',
-    'e2e-002',
-    Money::CHF(30000), // CHF 300.00
-    'Finanzverwaltung Stadt Musterhausen',
-    UnstructuredPostalAddress::sanitize('Altstadt 1a', '4998 Musterhausen'),
-    new PostalAccount('80-151-4')
-);
-
-$transaction = new ForeignCreditTransfer(
+$transaction1 = new ForeignCreditTransfer(
     '12323123123',                                                  // Уникальный id платежа
     '20',                                                           // Номер документа
     Money::RUB(5000000),                                            // Сумма в копейках
@@ -57,10 +32,27 @@ $transaction = new ForeignCreditTransfer(
     new GeneralAccount('40702810901300013927')                      // Корсчет банка получателя
 );
 
-$transaction->setServiceLevel(new ServiceLevelCode(ServiceLevelCode::CODE_NURG));   // Срочность
-$transaction->setRemittanceInformation('Обычный платеж физ лицу');                  // Комментарий
-$transaction->setCreditorReference('123');                                          // Код платежа (УИН)
-$transaction->setPurpose(new PurposeProprietary('5'));                              // Очередность платежа
+$transaction1->setServiceLevel(new ServiceLevelCode(ServiceLevelCode::CODE_NURG));   // Срочность
+$transaction1->setRemittanceInformation('Обычный платеж физ лицу');                  // Комментарий
+$transaction1->setCreditorReference('123');                                          // Код платежа (УИН)
+$transaction1->setPurpose(new PurposeProprietary('5'));                              // Очередность платежа
+
+$transaction2 = new ForeignCreditTransfer(
+    '12323123123',                                                  // Уникальный id платежа
+    '20',                                                           // Номер документа
+    Money::RUB(15000000),                                           // Сумма в копейках
+    'ООО "Мир технологий"',                                         // Наименование получателя
+    new UnstructuredPostalAddress(null, null, 'RU'),                // Адрес получателя
+    new Inn('7730189312'),                                          // ИНН получателя
+    new BBAN('40702810901300013927'),                               // Счет получателя
+    new RUBIC('123456789', 'АО "АЛЬФА-БАНК" Г МОСКВА', new UnstructuredPostalAddress(null, null, 'RU')),    // банк получателя
+    new GeneralAccount('40702810901300013927')                      // Корсчет банка получателя
+);
+
+$transaction2->setServiceLevel(new ServiceLevelCode(ServiceLevelCode::CODE_NURG));   // Срочность
+$transaction2->setRemittanceInformation('Обычный платеж юр лицуу');                  // Комментарий
+$transaction2->setCreditorReference('123');                                          // Код платежа (УИН)
+$transaction2->setPurpose(new PurposeProprietary('5'));                              // Очередность платежа
 
 $payment = new PaymentInformation(
     '7730189312_pain_PKG_20180521_00003',                           // Уникальный id пакета платежей
@@ -73,9 +65,8 @@ $payment = new PaymentInformation(
     new UnstructuredPostalAddress(null, null, 'RU')                 // почтовый адрес плательщика
 );
 $payment->setServiceLevel(new ServiceLevelCode(ServiceLevelCode::CODE_NURG));   // Срочность
-$payment->addTransaction($transaction);
-//$payment->addTransaction($transaction1);
-//$payment->addTransaction($transaction2);
+$payment->addTransaction($transaction1);
+$payment->addTransaction($transaction2);
 
 $message = new CustomerCreditTransfer(
     '7730189312_pain_MSG_20180521_00003',                           // id сообщения
